5e05dac1400139dfd6a54e0c9ebf9221cfaf4bcf
Alejandro <mariscalromeroalejandro@gmail.com>
2024-10-17 11:42:23 +0200

    test login

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,13 +1,16 @@
 import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
 import TopBar from '../pages/components/TopBar';
 
 describe('Example', () => {
   let homepage;
+  let loginPage;
   let topBar;
 
   beforeAll(async () => {
     jest.setTimeout(15000);
     homepage = new HomePage();
+    loginPage = new LoginPage();
     topBar = new TopBar();
   });
 
@@ -18,5 +21,11 @@ describe('Example', () => {
   it('navbar should be displayed', async () => {
     await homepage.isNavbarDisplayed();
     await topBar.isTopBarDisplayed();
+  });
+
+  it('should login', async () => {
+    await loginPage.visit();
+    await loginPage.isLoginFormDisplayed();
+    await loginPage.login('username', 'password');
   });
 });
